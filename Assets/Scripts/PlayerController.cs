560a3630ddc961c755b4d79d40e63a2f5e60af47
qnvwomp <qnvwomp@stu.ccsdut.org>
2024-04-30 09:09:00 -0600

    movement script
    tried and failed to add a movement script lol

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
 
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+	public float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private float movementX;
+	private float movementY;
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+	private Rigidbody rb;
+	private int count;
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
 
-        rb.AddForce(movement * speed); 
-    }
+		count = 0;
+
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+		rb.AddForce(movement * speed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+
+			count = count + 1;
+
+			SetCountText();
+		}
+	}
+
+	void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+
+	}
 }
